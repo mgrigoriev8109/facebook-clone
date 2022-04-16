ec721a7d84c0556b5eddbb30bb002f340f2033a0
mikhail <mikhailgrigoriev92@gmail.com>
2022-04-16 09:41:17 -0400

    Work on implementing devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  Rails.application.routes.draw do
+    devise_for :users, controllers: {
+      sessions: 'users/sessions'
+    }
+  end
+
+  resources :users, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
