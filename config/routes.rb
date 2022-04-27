cc0021b9150e5614029c00295ee52785781335b6
mikhail <mikhailgrigoriev92@gmail.com>
2022-04-27 11:07:49 -0400

    Fix root route redirect to log-in
    If user logged out, using authenticate_user! within the PostController, which is the root controller/view

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  authenticated(:user) do
-    root "posts#index", as: :authenticated_root
-  end
-  root "posts#new"
+
+  root "posts#index"
 
 end
