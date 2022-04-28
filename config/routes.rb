e6f3a17c4ab54929b25d0f2656996267c5b1139e
mikhail <mikhailgrigoriev92@gmail.com>
2022-04-28 10:14:59 -0400

    Change Route root to User profile
    Creating a boilerplate view, and UsersController with show action that needs work to be functional

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,9 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
 
+  resources :users, only: [:show]
   resources :posts
-  #resources :users, only: [:index]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-
-  root "posts#index"
+  root "users#show"
 
 end
