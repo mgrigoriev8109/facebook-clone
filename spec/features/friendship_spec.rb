c5018cce793b7438ac76ccca468b01e5f5727ea2
mikhail <mikhailgrigoriev92@gmail.com>
2022-04-30 13:32:05 -0400

    Install database cleaner

--- spec/features/friendship_spec.rb
+++ spec/features/friendship_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
+require 'database_cleaner/active_record'
 
 RSpec.describe 'Friendship Integration Tests', type: :system do
+  before(:each) do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean  
+  end
 
   it 'Verifies a sent friendship request' do
     user_1 = FactoryBot.create(:user, email: '1@gmail.com', username: 'user_1')
