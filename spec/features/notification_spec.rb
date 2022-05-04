47207b5f205b1d340a5a54b9e65b47d4a200f549
mikhail <mikhailgrigoriev92@gmail.com>
2022-05-04 12:58:06 -0400

    Pass test for Notification creation at friendship acceptance

--- spec/features/notification_spec.rb
+++ spec/features/notification_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'Notification Integration Tests', type: :system do
     login_as(user_1, :scope => :user) 
   end
 
-  it 'Verifies notification creation upon friend request acceptance' do
+  it 'Verifies notification creation upon friend request creation' do
     visit users_path
     click_on 'user_2'
     click_on 'Request Friendship'
@@ -21,5 +21,22 @@ RSpec.describe 'Notification Integration Tests', type: :system do
     visit notifications_path
 
     expect(page).to have_content('user_1 sent you a friendship request!')
+  end
+
+  it 'Verifies notification creation upon friendship creation' do
+    visit users_path
+    click_on 'user_2'
+    click_on 'Request Friendship'
+    click_on "Sign Out"
+
+    login_as(user_2, :scope => :user)
+    visit friendship_requests_path
+    click_on "Accept Friendship"
+    click_on "Sign Out"
+
+    login_as(user_1, :scope => :user)
+    visit notifications_path
+
+    expect(page).to have_content('user_2 accepted your friendship request!')
   end
 end
