ca5c4fb4966d6c93c50adb17f243b9bbafdc2974
mikhail <mikhailgrigoriev92@gmail.com>
2022-05-07 13:32:41 -0400

    Fix comment creation in user profile
    Also removing unnecesasry partial for new user posts.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
-    @posts = Post.all
+    @posts = Post.where(user_id: current_user.id)
     @friendship_request = FriendshipRequest.new
+    @comment = Comment.new
   end
 
   def index
