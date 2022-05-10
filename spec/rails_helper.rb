dc74ed0afdc72fd0a7152965e181f389ef5ffead
mikhail <mikhailgrigoriev92@gmail.com>
2022-05-10 11:54:32 -0400

    Begin incorporating tailwind to user login

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ require 'devise'
 require_relative './support/controller_macros'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+Capybara.javascript_driver = :selenium_chrome_headless
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
