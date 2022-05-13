4911922f430977df9f8b0e955a05da14cf36659e
mikhail <mikhailgrigoriev92@gmail.com>
2022-05-13 11:20:23 -0400

    Fix omniauth github login signup
    Missing steps were app registration and figaro usage

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
   
   #has_many :friendships
   has_many :posts
@@ -31,7 +31,6 @@ class User < ApplicationRecord
   has_many :friendship_requests
   has_many :likes
 
-  devise :omniauthable, omniauth_providers: %i[github]
   def user_has_new_notifications
     notifications = Notification.all
     any_unviewed_notifications = false
@@ -46,9 +45,11 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      #user.provider = auth.provider
+      #user.uid = auth.id
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      user.username = auth.info.name   # assuming the user model has a name
+      #user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+    provider :github, ENV["pusher_key"], ENV['pusher_secret']
   end
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
@@ -295,8 +295,6 @@ Devise.setup do |config|
   #
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
-  #config.omniauth_path_prefix = '/my_engine/users/auth'
-
   # ==> Turbolinks configuration
   # If your app is using Turbolinks, Turbolinks::Controller needs to be included to make redirection work correctly:
   #
